5d0b03e13527ea9110fd192d74937e83d9481667
divyanshshrivastava <devshri19056@gmail.com>
2025-09-24 21:12:27 +0530

    Added show route through RESTful API's

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const Listing = require("./models/listing");
+const path = require("path");
+
+main()
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Root is working");
+});
+
+//*Index Route
+app.get("/listing", async (req, res) => {
+  let allListing = await Listing.find({});
+  console.log(allListing[1].image);
+  res.render("./listings/index.ejs", { allListing });
+});
+
+//*SHOW Route
+app.get("/listing/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  const cardInfo = await Listing.findById(id);
+  console.log(cardInfo);
+  res.render("./listings/showInfo.ejs", { cardInfo });
+});
+
+//*this is a route for testing DB connection
+/* 
+app.get("/testListing", async (req, res) => {
+  let sample = new Listing({
+    title: "My Seafacing Villa",
+    description: "This sea facing will show you the best view in all of Goa",
+    price: 25000,
+    location: "Calangute, Goa",
+    country: "India",
+  }
+  await sample.save();
+  res.send("sample Listing added");
+});
+*/
+
+app.listen(8080, (req, res) => {
+  console.log("server running on port:8080");
+});
---
+++ models/listing.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1449824913935-59a10b8d2000?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1449824913935-59a10b8d2000?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: {
+    type: Number,
+    min: 100,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
