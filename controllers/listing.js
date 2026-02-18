b183cc512bb9ff1b56ac4931502f8ac8dc9549e9
divyanshshrivastava <devshri19056@gmail.com>
2026-02-18 14:22:28 +0530

    Enhance listing edit functionality: add image upload support and improve form layout

--- controllers/listing.js
+++ controllers/listing.js
@@ -47,12 +47,22 @@ module.exports.renderEditForm = async (req, res) => {
     req.flash("error", "Listing does not Exist");
     return res.redirect("/listings");
   }
-  res.render("./listings/edit.ejs", { cardInfo });
+
+  let originalImageUrl = cardInfo.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+
+  res.render("./listings/edit.ejs", { cardInfo, originalImageUrl });
 };
 
 module.exports.editListing = async (req, res) => {
   let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { url, filename };
+    await listing.save();
+  }
   req.flash("success", "Listing Updated Successfully");
   res.redirect(`/listings/${id}`);
 };
